3da0592889eed2c44cf8371bf15d88ea5a345d9a
Helmut Puhr <helmut.puhr@gmx.at>
2020-02-26 18:59:57 +0100

    detection type improvement

--- analyze/detection_type.py
+++ analyze/detection_type.py
@@ -69,7 +69,7 @@ class TrackStatisticsCalculator:
     def diff_det_cnt_sum(self):
         return self._diff_det_cnt_sum
 
-    def get_detection_type_verif2(self, record):
+    def get_detection_type(self, record):
 
         if find_value(["080", "CST"], record) == 1:
             return 0  # no detection
@@ -79,110 +79,9 @@ class TrackStatisticsCalculator:
         mds_updated = find_value(["080", "MDS"], record) == 0
         ads_updated = find_value(["080", "ADS"], record) == 0
 
-        # '062.290.MDS.Age' count 3056 (100%) min '0.0' max '63.75'
-        # '062.290.MLT.Age' count 3056 (100%) min '0.0' max '63.75'
-        # '062.290.PSR.Age' count 3056 (100%) min '0.0' max '63.75'
-        # '062.290.SSR.Age' count 3056 (100%) min '0.0' max '63.75'
+        if not mds_updated:
+            if psr_updated and not ssr_updated:
 
-
-        is_mode_s = mds_updated
-
-        #if mds_updated or ads_updated:
-        #    is_mode_s = True
-        #else:
-        #    #is_mode_s = find_value(["380", "ADR", "Target Address"], record) is not None
-        #    is_mode_s = find_value(["290", "MDS", "Age"], record) <= 4.0 \
-        #                or find_value(["290", "MLT", "Age"], record) <= 4.0
-
-        is_ssr = ssr_updated
-
-        #if ssr_updated:
-        #    is_ssr = True
-        #if not is_mode_s:
-        #    is_ssr = find_value(["060", "Mode-3/A reply"], record) is not None \
-        #             or find_value(["136", "Measured Flight Level"], record) is not None
-
-        is_psr = psr_updated
-
-        if not is_mode_s:
-            if is_psr and not is_ssr:
-
-                #if find_value(["290", "SSR", "Age"], record) <= 4.0:
-                if find_value(["290", "MLT", "Age"], record) is not None:
-
-                    # age not 63.75
-                    mlat_age = find_value(["290", "MLT", "Age"], record)
-
-                    mda_age = find_value(["295", "MDA", "Age"], record)
-                    mda_age_found = mda_age is not None
-
-                    mdc_age = find_value(["295", "MFL", "Age"], record)
-                    mdc_age_found = mdc_age is not None
-
-                    if mlat_age <= 10.0:
-                        if mda_age_found and mda_age <= 26.0 \
-                                and mdc_age_found and mdc_age <= 26.0:
-                            return 3
-
-                        if not mda_age_found and mdc_age_found and mdc_age <= 8.0:
-                            return 3
-
-                        #if mda_age_found and mda_age <= mlat_age:
-                        #    return 3
-
-                        #if mdc_age_found and mdc_age <= mlat_age:
-                        #    return 3
-
-                        #if not mda_age_found and mdc_age_found and mdc_age <= 10.0:
-                        #    return 3
-
-                        #if (mda_age_found and mda_age == mlat_age) \
-                        #        or (mdc_age_found and mdc_age == mlat_age):
-                        #    return 3
-                #else:
-                return 1  # single psr, no mode-s
-
-            if not is_psr and is_ssr:
-                return 2  # single ssr, no mode-s
-
-            if is_psr and is_ssr:
-                return 3  # cmb, no mode-s
-
-        else:
-            if not is_psr:
-                return 5  # ssr, mode-s
-            else:
-                return 7  # cmb, mode-s
-
-        #if find_value(["290", "MLT", "Age"], record) <= 4.0:
-        if find_value(["380", "ADR", "Target Address"], record) is not None:
-            return 5
-
-        if find_value(["060", "Mode-3/A reply"], record) is not None \
-                or find_value(["136", "Measured Flight Level"], record) is not None:
-            return 2
-
-        #print('unknown')
-        return 0  # unknown
-
-    def get_detection_type_verif(self, record):
-
-        if find_value(["080", "CST"], record) == 1:
-            return 0  # no detection
-
-        psr_updated = find_value(["080", "PSR"], record) == 0
-        ssr_updated = find_value(["080", "SSR"], record) == 0
-        mds_updated = find_value(["080", "MDS"], record) == 0
-        ads_updated = find_value(["080", "ADS"], record) == 0
-
-        is_mode_s = mds_updated
-        is_ssr = ssr_updated
-        is_psr = psr_updated
-
-        if not is_mode_s:
-            if is_psr and not is_ssr:
-
-                #if find_value(["290", "SSR", "Age"], record) <= 4.0:
                 if find_value(["290", "MLT", "Age"], record) is not None:
                     # age not 63.75
                     mlat_age = find_value(["290", "MLT", "Age"], record)
@@ -190,29 +89,16 @@ class TrackStatisticsCalculator:
                     if mlat_age <= 12.0:
                         return 3
 
-                #
-                #     mda_age = find_value(["295", "MDA", "Age"], record)
-                #     mda_age_found = mda_age is not None
-                #
-                #     mdc_age = find_value(["295", "MFL", "Age"], record)
-                #     mdc_age_found = mdc_age is not None
-                #
-                #         if mda_age_found and mda_age <= 26.0 \
-                #                 and mdc_age_found and mdc_age <= 26.0:
-                #
-                #         if not mda_age_found and mdc_age_found and mdc_age <= 8.0:
-                #             return 3
-
                 return 1  # single psr, no mode-s
 
-            if not is_psr and is_ssr:
+            if not psr_updated and ssr_updated:
                 return 2  # single ssr, no mode-s
 
-            if is_psr and is_ssr:
+            if psr_updated and ssr_updated:
                 return 3  # cmb, no mode-s
 
         else:
-            if not is_psr:
+            if not psr_updated:
                 return 5  # ssr, mode-s
             else:
                 return 7  # cmb, mode-s
@@ -226,12 +112,11 @@ class TrackStatisticsCalculator:
 
         return 0  # unknown
 
-
     def process_record(self, cat, record):
 
         self.__num_records += 1
 
-        sdl_detection_type = self.get_detection_type_verif(record)
+        sdl_detection_type = self.get_detection_type(record)
 
         if sdl_detection_type == 1:
             self.__num_psr_records += 1
@@ -255,7 +140,7 @@ class TrackStatisticsCalculator:
 
             key = (sdl_detection_type, db_detection_type)
 
-            print(' UGA sdl det {} verif {} PSR {} SSR {} MDS {} MLT {} ADS {} MA {} MF {}'.format(
+            print('\tdifference det {} verif {} ages PSR {} SSR {} MDS {} MLT {} ADS {} MA {} MF {}'.format(
                 sdl_detection_type, db_detection_type,
                 find_value(["290", "PSR", "Age"], record),
                 find_value(["290", "SSR", "Age"], record),
@@ -266,14 +151,14 @@ class TrackStatisticsCalculator:
                 find_value(["295", "MFL", "Age"], record)
             ))
 
-            #print('rec_num {} sdl_det {} ({})  v7_det {} ({}) json \'{}\''.format(
+            #print('rec_num {} det {} ({})  v7_det {} ({}) json \'{}\''.format(
             #    db_rec_num,
             #    sdl_detection_type, detection_type_strings[sdl_detection_type],
             #    db_detection_type, detection_type_strings[db_detection_type],
             #    json.dumps(record, indent=4)))
 
             if key not in self._diff_det_cnt:
-                print('rec_num {} sdl_det {} ({})  v7_det {} ({}) json \'{}\''.format(
+                print('rec_num {} det {} ({}) v7_det {} ({}) json \'{}\''.format(
                     db_rec_num,
                     sdl_detection_type, detection_type_strings[sdl_detection_type],
                     db_detection_type, detection_type_strings[db_detection_type],
