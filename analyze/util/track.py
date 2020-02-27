06d665d77df96bf3dc2677c46ac8773c8de26547
Helmut Puhr <helmut.puhr@gmx.at>
2020-02-27 12:19:03 +0100

    preparation for track mapping checks

---
+++ analyze/util/track.py
@@ -0,0 +1,43 @@
+from util.common import find_value
+
+def get_detection_type(record):
+    if find_value("080.CST", record) == 1:
+        return 0  # no detection
+
+    psr_updated = find_value("080.PSR", record) == 0
+    ssr_updated = find_value("080.SSR", record) == 0
+    mds_updated = find_value("080.MDS", record) == 0
+    ads_updated = find_value("080.ADS", record) == 0
+
+    if not mds_updated:
+        if psr_updated and not ssr_updated:
+
+            if find_value("290.MLT.Age", record) is not None:
+                # age not 63.75
+                mlat_age = find_value("290.MLT.Age", record)
+
+                if mlat_age <= 12.0:
+                    return 3
+
+            return 1  # single psr, no mode-s
+
+        if not psr_updated and ssr_updated:
+            return 2  # single ssr, no mode-s
+
+        if psr_updated and ssr_updated:
+            return 3  # cmb, no mode-s
+
+    else:
+        if not psr_updated:
+            return 5  # ssr, mode-s
+        else:
+            return 7  # cmb, mode-s
+
+    if find_value("380.ADR.Target Address", record) is not None:
+        return 5
+
+    if find_value("060.Mode-3/A reply", record) is not None \
+            or find_value("136.Measured Flight Level", record) is not None:
+        return 2
+
+    return 0  # unknown
